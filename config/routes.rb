01081671f062875cb156327bb3b2440ca172bb0c
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-03-13 06:53:05 +0000

    [Add]3-12 top画面の作成と設定の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'lists#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
+  resources :books
+  # get 'books/new'
+  # post 'books' => 'books#create'
+  # get 'books' => 'books#index'
+  # get 'books/:id' => 'books#show'
+  # get 'books/edit'
+  # get '/top' => 'homes#top'
 
-  get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
